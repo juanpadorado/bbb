bab5a6d527860c71d97a7b2dc156ee4f089aca58
juanpadorado <juanpadorado@gmail.com>
2015-05-01 16:32:14 -0500

    Proyecto
    Creacion de la carpeta del proyecto

---
+++ QUEHACEMOSCALI/app/src/androidTest/java/juan/pablo/dorado/quehacemoscali/ApplicationTest.java
@@ -0,0 +1,13 @@
+package juan.pablo.dorado.quehacemoscali;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
